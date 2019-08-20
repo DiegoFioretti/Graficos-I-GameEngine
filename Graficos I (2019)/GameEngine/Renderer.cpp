5c5c0e6f2a2873c721e6d35b1d5e9ca3bbcf0f9c
diego <diego_fioretti_97@hotmail.com>
2019-08-20 12:59:08 -0300

    Integracion de GLEW (Broken As Fuck)
    +Hay que arreglar GLEW asi dibuja un triangulo, si se puede entonces empezar Shape

--- Graficos I (2019)/GameEngine/Renderer.cpp
+++ Graficos I (2019)/GameEngine/Renderer.cpp
@@ -2,6 +2,24 @@
 
 Renderer::Renderer()
 {
+	glewInit();
+
+	static const GLfloat vertices[] = {
+		0.0f, 1.0f, 0.0f,
+		-1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+	};
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
 
@@ -12,7 +30,16 @@ Renderer::~Renderer()
 void Renderer::WindowRefresh(GLFWwindow* window)
 {
 	/* Render here */
+
+	glClearColor(0.62, 0.97, 0.34, 1);
+
+	glClearColor(0.82, 0.420, 0.69, 0.3);
+
+	//glColor4f(0.82, 0.420, 0.69, 0.3);
+
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	/* Swap front and back buffers */
 	glfwSwapBuffers(window);
--- Graficos I (2019)/GameEngine/Renderer.h
+++ Graficos I (2019)/GameEngine/Renderer.h
@@ -4,6 +4,8 @@
 
 #include "Window.h"
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <GL/GL.h>
 
 class Renderer
 {
