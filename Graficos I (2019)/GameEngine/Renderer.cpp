92f81099c0b3c92c27cf3c5b53218b5b9cd89669
D <diego_fioretti_97@hotmail.com>
2019-10-15 13:01:45 -0300

    Inicio de Texturas
    Hay que hacer que se pueda ver la textura
    Hay que tener métodos virtuales
    La solución funciona en VS2019

--- Graficos I (2019)/GameEngine/Renderer.cpp
+++ Graficos I (2019)/GameEngine/Renderer.cpp
@@ -5,14 +5,16 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <chrono>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // Shader sources
 const GLchar* vertexSource = R"glsl(
-    #version 150 core
+    #version 330 core
 
-in vec2 position;
-in vec3 color;
-in vec2 texcoord;
+layout (location = 0) in vec2 position;
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texcoord;
 
 out vec3 Color;
 out vec2 Texcoord;
@@ -27,12 +29,17 @@ void main()
 }
 )glsl";
 const GLchar* fragmentSource = R"glsl(
-    #version 150 core
+    #version 330 core
     in vec3 Color;
+	in vec2 textCoord;
+
     out vec4 outColor;
+
+	uniform sampler2D myTexture;
+
     void main()
     {
-        outColor = vec4(Color, 1.0);
+        outColor = texture(myTexture, textCoord) * vec4(Color, 1.0);
     }
 )glsl";
 
@@ -56,10 +63,10 @@ Renderer::Renderer()
 
 	GLfloat vertices[] = {
 		//  Position            Color             Texcoords
-			-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
-			 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
-			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
-			-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
+			-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Top-left
+			 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, // Top-right
+			 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // Bottom-right
+			-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f  // Bottom-left
 	};
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -76,7 +83,7 @@ Renderer::Renderer()
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
-
+//-------------------------------------------SHADERS-------------------------------------------------------
 	// Create and compile the vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
@@ -94,7 +101,40 @@ Renderer::Renderer()
 	glBindFragDataLocation(shaderProgram, 0, "outColor");
 	glLinkProgram(shaderProgram);
 	glUseProgram(shaderProgram);
+//-------------------------------------------TEXTURES-------------------------------------------------------
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("../texture.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		std::cout << "Width "<< width << std::endl;
+		std::cout << "Height "<< height<< std::endl;
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//-------------------------------------------TRANSFORM-------------------------------------------------------
 	// Specify the layout of the vertex data
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
 	glEnableVertexAttribArray(posAttrib);
