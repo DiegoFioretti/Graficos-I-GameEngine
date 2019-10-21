2cb2b323895d16728aa1a021199e62d81a6563c3
Nico <diego_fioretti_97@hotmail.com>
2019-10-21 00:42:41 -0300

    Inicio de Entity y Sprite
    Creado los archivos .h y .cpp de Entity y Sprite

---
+++ Graficos I (2019)/GameEngine/Entity.h
@@ -0,0 +1,11 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+#pragma once
+
+#include "Shape.h"
+#include "Sprite.h"
+
+class Entity
+{
+};
+#endif
---
+++ Graficos I (2019)/GameEngine/Sprite.h
@@ -0,0 +1,7 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+#pragma once
+class Sprite
+{
+};
+#endif
